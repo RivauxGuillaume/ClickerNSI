fef566c8bb0e7b7e0a0bbe81da5f89ccd6092d72
hp-fort <96954861+hp-fort@users.noreply.github.com>
2024-03-08 13:06:25 +0100

    branche pop up, en cours de dev, pb sur le label de début, pas encore de liens pour le changement

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 commentaire pour l'autre ici
 """
 import time
-from tkinter import Tk, Canvas, Label, Button
+from tkinter import Tk, Canvas, Label, Button, Text
 import threading
 
 
@@ -14,44 +14,61 @@ def creer_fenetre():
     fenetre.title("Music Clicker")
     return fenetre
 
+def creer_Canvas():
+    zone_graphique = Canvas(fenetre, width=900, height=500, bg='black')
+    zone_graphique.grid(row=0, column=0, rowspan=15, columnspan=15)
+    return zone_graphique
 
 def creer_button():
     bouton_clicker = Button(fenetre, text="Clique !!!", width=50, height=20, bg="black", fg="white", bd=0, activebackground="black", command=AjoutScore, borderwidth=5) #remplacer le clicker qui est actuelement un bouton par une image (qui si possible change au fur a mesur du niveau)
-    bouton_clicker.grid(row=5, column=5)
+    bouton_clicker.grid(row=6, column=5)
     #Création des différents bouton d'améliorations
     Amelioration1Button = Button(fenetre, text="Acheter", width=10, height=1, bg="white", command=Amelioration1Clic)
-    Amelioration1Button.grid(row=1, column=14)
+    Amelioration1Button.grid(row=2, column=14)
 
     Amelioration2Button = Button(fenetre, text="Acheter", width=10, height=1, bg="white", command=Amelioration2Clic)
-    Amelioration2Button.grid(row=2, column=14)
+    Amelioration2Button.grid(row=3, column=14)
     return bouton_clicker, Amelioration1Button, Amelioration2Button
 
 
 def creer_text():
     # Création des différents textes d'améliorations
     Amelioration1Text=Label(fenetre, text=f"Amelioration 1 :  {prix1} notes", bg="black", fg="white")
-    Amelioration1Text.grid(row=1, column=13)
+    Amelioration1Text.grid(row=2, column=13)
 
     Amelioration2Text=Label(fenetre, text=f"Amelioration 1 :  {prix2} notes", bg="black", fg="white")
-    Amelioration2Text.grid(row=2, column=13)
+    Amelioration2Text.grid(row=3, column=13)
+
     # Création du texte du score
     texte_score=Label(fenetre, text=f"{score} notes", bg="black", fg="white")
-    texte_score.grid(row=0, column=5)
+    texte_score.grid(row=1, column=5)
+    
     # Création du nb de clic
     texte_scoreClick=Label(fenetre, text=f"{scoreClick} notes/Clics", bg="black", fg="white")
-    texte_scoreClick.grid(row=1, column=5)
+    texte_scoreClick.grid(row=2, column=5)
+    
     # Création du texte du score/sec
     texte_clicSec=Label(fenetre, text=f"{scoreSec} notes/Secondes", bg="black", fg="white")
-    texte_clicSec.grid(row=2, column=5)
+    texte_clicSec.grid(row=3, column=5)
 
+    # Création de l'entête affichant le joueur qui joue
+    text_joueur = Label(fenetre, "Joueur : ", bg="black", fg="white")
+    text_joueur.grid(row=0, column=6, columnspan=2)
 
     return Amelioration1Text, Amelioration2Text, texte_score, texte_clicSec, texte_scoreClick
 
+def pop_up():
+    fenetre_pop_up = Tk()
+    fenetre_pop_up.title("Entre ton nom")
+    fenetre_pop_up.geometry("300x200")
+    text_popup = Label(fenetre_pop_up, "Entre ton nom : ", height=11, ).grid(row=0, column=0)
+    prenom_user = Text(fenetre_pop_up, height=1, width=14)
+    prenom_user.grid(row=0, column=1)
+    joueur = prenom_user.get("1.0", "end-1c")
 
-def creer_Canvas():
-    zone_graphique = Canvas(fenetre, width=900, height=500, bg='black')
-    zone_graphique.grid(row=0, column=0, rowspan=15, columnspan=15)
-    return zone_graphique
+
+    return fenetre_pop_up, text_popup, prenom_user, joueur
+    
 
 
 def AjoutScore():
@@ -128,8 +145,9 @@ score=0
 fenetre=creer_fenetre()
 zone_graphique=creer_Canvas()
 bouton_clicker, Amelioration1Button, Amelioration2Button=creer_button()
-
 Amelioration1Text, Amelioration2Text, texte_score, texte_clicSec, texte_scoreClick=creer_text()
+fenetre_pop_up, text_popup, prenom_user, joueur = pop_up()
+
 
 # activation commandes admin
 fenetre.bind("<Up>", point1k)
@@ -142,8 +160,6 @@ th1.start()
 
 
 fenetre.mainloop()
-
-
 
 
 
