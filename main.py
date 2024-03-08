c2d1fde476c8eaa64f9ec95e54ca4f6674df6183
hp-fort <96954861+hp-fort@users.noreply.github.com>
2024-03-08 11:57:13 +0100

    ajout d'une branche pour le nom de l'utilisateur, pas reussi a couper le thread ouvert a la fin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 commentaire pour l'autre ici
 """
 import time
-from tkinter import Tk, Canvas, Label, Button
+from tkinter import Tk, Canvas, Label, Button, Text
 import threading
 
 
@@ -17,36 +17,35 @@ def creer_fenetre():
 
 def creer_button():
     bouton_clicker = Button(fenetre, text="Clique !!!", width=50, height=20, bg="black", fg="white", bd=0, activebackground="black", command=AjoutScore, borderwidth=5) #remplacer le clicker qui est actuelement un bouton par une image (qui si possible change au fur a mesur du niveau)
-    bouton_clicker.grid(row=5, column=5)
+    bouton_clicker.grid(row=6, column=5)
     #Création des différents bouton d'améliorations
     Amelioration1Button = Button(fenetre, text="Acheter", width=10, height=1, bg="white", command=Amelioration1Clic)
-    Amelioration1Button.grid(row=1, column=14)
+    Amelioration1Button.grid(row=2, column=14)
 
     Amelioration2Button = Button(fenetre, text="Acheter", width=10, height=1, bg="white", command=Amelioration2Clic)
-    Amelioration2Button.grid(row=2, column=14)
+    Amelioration2Button.grid(row=3, column=14)
     return bouton_clicker, Amelioration1Button, Amelioration2Button
 
 
 def creer_text():
     # Création des différents textes d'améliorations
     Amelioration1Text=Label(fenetre, text=f"Amelioration 1 :  {prix1} notes", bg="black", fg="white")
-    Amelioration1Text.grid(row=1, column=13)
+    Amelioration1Text.grid(row=2, column=13)
 
     Amelioration2Text=Label(fenetre, text=f"Amelioration 1 :  {prix2} notes", bg="black", fg="white")
-    Amelioration2Text.grid(row=2, column=13)
+    Amelioration2Text.grid(row=3, column=13)
     # Création du texte du score
     texte_score=Label(fenetre, text=f"{score} notes", bg="black", fg="white")
-    texte_score.grid(row=0, column=5)
+    texte_score.grid(row=1, column=5)
     # Création du nb de clic
     texte_scoreClick=Label(fenetre, text=f"{scoreClick} notes/Clics", bg="black", fg="white")
-    texte_scoreClick.grid(row=1, column=5)
+    texte_scoreClick.grid(row=2, column=5)
     # Création du texte du score/sec
     texte_clicSec=Label(fenetre, text=f"{scoreSec} notes/Secondes", bg="black", fg="white")
-    texte_clicSec.grid(row=2, column=5)
+    texte_clicSec.grid(row=3, column=5)
 
 
     return Amelioration1Text, Amelioration2Text, texte_score, texte_clicSec, texte_scoreClick
-
 
 def creer_Canvas():
     zone_graphique = Canvas(fenetre, width=900, height=500, bg='black')
@@ -63,11 +62,13 @@ def AjoutScore():
 
 def MajScoreSec():
     global score
-    while True:
+    while close == False:
         if scoreSec != 0 :
             time.sleep(1/scoreSec)
             score+=1
             texte_score.configure(text=f"{score} notes")
+            if close == True:
+                raise SystemExit()
 
 
 def Amelioration1Clic():
@@ -99,6 +100,7 @@ def Amelioration3Clic():
         score=score-1000                                      # A modifier
         texte_score.configure(text=f"{score} notes")
 #---------------------------------------------------------Provisoire---------------------------------------
+
 def point1k(event):
     global score
     score = 1000
@@ -128,7 +130,6 @@ score=0
 fenetre=creer_fenetre()
 zone_graphique=creer_Canvas()
 bouton_clicker, Amelioration1Button, Amelioration2Button=creer_button()
-
 Amelioration1Text, Amelioration2Text, texte_score, texte_clicSec, texte_scoreClick=creer_text()
 
 # activation commandes admin
@@ -137,13 +138,10 @@ fenetre.bind("<Left>", ptpclick)
 fenetre.bind("<Right>", ptpsecondes)
 
 # Gestion du multithread pour les clic/sec
+close = False
 th1=threading.Thread(target=MajScoreSec)
 th1.start()
 
-
 fenetre.mainloop()
-
-
-
-
+close = True
 
